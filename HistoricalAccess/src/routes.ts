2553f1aa56d41c78f7970b89124930eb6b5c174c
ethanshry <ehshry@gmail.com>
2018-09-22 11:35:14 -0500

    react funcitonal

--- HistoricalAccess/src/routes.ts
+++ HistoricalAccess/src/routes.ts
@@ -3,7 +3,11 @@ import { Express, Request, Response } from 'express';
 export function registerRoutes(app: Express) {
 
     app.get('/ping', (req: Request, res: Response) => {
-        res.send('ping');
+        res.send('pong');
+    });
+
+    app.get('/asset/:id', (req: Request, res: Response) => {
+        res.send('asset: ' + req.params.id);
     });
 
 }
